e0493c42f8e6e4985151ea976818f82c1049f4ee
root <root@DESKTOP-HC7G8IA>
2025-03-18 11:00:40 +0200

    Initial commit

---
+++ src/components/StickyHeader/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.png';
+
+function Header() {
+  return (
+        <>
+        <header className ="sticky-header">
+            <img src="{logo}" alt="Logo" />
+            <nav>
+                <form action="">
+                <input type='text' id='searchbar' name='searchbar' placeholder="Search" />
+                </form>
+                <img src="#" alt="bookmark" />
+                <img src="#" alt="userimg" />
+                <img src="#" alt="shopingbag" />
+                
+            </nav>
+        </header>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam adipisci minus aut dolore culpa beatae accusamus facere placeat et unde cum voluptatibus natus praesentium amet aliquid, eos perspiciatis nostrum eaque exercitationem, optio doloremque illum iste? Natus officia blanditiis explicabo consectetur laborum, dicta esse velit saepe quas. Vel placeat, repellendus rerum vitae consectetur eligendi natus odio veniam alias. Maiores esse, quisquam itaque illo placeat adipisci! Mollitia, minus. Qui earum doloribus iste dolorum facere laboriosam soluta eveniet in? Maiores nisi sapiente, eos rem minus corporis quaerat. Pariatur odio tempora accusantium expedita soluta iusto ullam commodi cum harum temporibus, maxime similique in? Qui itaque animi pariatur architecto perspiciatis quaerat molestias dolore aperiam dolor alias. Saepe praesentium illum dignissimos magnam, provident et tempora iste! Quod laboriosam quo omnis blanditiis aperiam nulla culpa officiis suscipit expedita eum? Deleniti temporibus voluptates est, quae, enim aut officia excepturi, libero iure aliquid ipsa asperiores dicta doloribus maxime fugit dolore non! Harum dolorem itaque aspernatur modi doloremque minus labore veritatis sed! Itaque ex ratione, obcaecati similique debitis doloremque non enim architecto numquam quod quasi, asperiores placeat ipsum sit dignissimos explicabo necessitatibus molestias eligendi at modi laudantium illo aperiam eos? Qui impedit quaerat numquam ut ullam nihil consectetur dicta molestias.
+        </p>
+        </>
+  );
+}
+
+export default Header;
